f75f3fc3c5398c280ad6a3c8cf0bc75dc5a6881c
Kyle <kylejordanwilliams@gmail.com>
2023-05-05 16:13:05 +0100

    Finished with buttons

--- ecomms/src/Components/Product/Product.js
+++ ecomms/src/Components/Product/Product.js
@@ -2,22 +2,27 @@ import React from "react";
 import "./product.css";
 
 function Product({ foundItems }) {
-
-  if(foundItems)
-  {
+  if (foundItems) {
     return (
-        <>
-      {foundItems.map((item, key) => {
-        return (
-          <div className="productContent">
-            <img className="ecomImg" src={item.image} alt={item.discription} />
-            <p key={key.id} className="prod-title">{item.title}</p>
-            <p>£ {item.price}</p>
-          </div>
-        );
-      })}
-    </>
-    )
+      <>
+        {foundItems.map((item, key) => {
+          return (
+            <div className="productContent">
+              <img
+                className="ecomImg"
+                src={item.image}
+                alt={item.discription}
+              />
+              <p key={key.id} className="prod-title">
+                {item.title}
+              </p>
+              <p>£ {item.price}</p>
+              <button className="btn">Add to cart</button>
+            </div>
+          );
+        })}
+      </>
+    );
   }
 }
 
