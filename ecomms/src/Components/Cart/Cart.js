fc185b58c50a913c30f5ac6596c4bc500bb4109f
Kyle <kylejordanwilliams@gmail.com>
2023-05-06 01:59:58 +0100

    Done

--- ecomms/src/Components/Cart/Cart.js
+++ ecomms/src/Components/Cart/Cart.js
@@ -1,16 +1,30 @@
-function Cart({ totalPrice, cartItems, handleClearCart }) {
+// import Product from "../Product/Product";
+
+function Cart({ totalPrice, cartItems, handleClearCart, item }) {
   return (
-    <div>
-      <h2>Cart</h2>
-      <ul>
-        {cartItems.map((item) => (
-          <li key={item.id}>{item.title}</li>
-        ))}
-      </ul>
-      <p>{cartItems.length} items</p>
-      <p>£ {totalPrice}</p>
-      <button onClick={handleClearCart}>Clear Cart</button>
-    </div>
+    <>
+      <a href="/">
+        Home
+        <img id="cartImg" src="/Assets/home.png" alt="home" />
+      </a>
+      <div>
+        <h2>Cart</h2>
+        <ul>
+          {cartItems.map((item) => (
+            <li key={item.id}>
+              {item.title}
+              <img src={item.image} alt={item.description} />
+            </li>
+          ))}
+        </ul>
+        <p>{cartItems.length} items</p>
+        <p>£ {totalPrice}</p>
+        <div className="clear-buyButtons">
+          <button onClick={handleClearCart}>Clear Cart</button>
+          <button>Buy Now</button>
+        </div>
+      </div>
+    </>
   );
 }
 
